ae7945bce26db4b3a39ab44730aab1d41f9473b2
Gurbani Saroy <gurbanisaroy@gmail.com>
2025-12-08 22:40:56 +0530

    changed font-sizes and bg color

--- frontend/src/components/Dashboard/DBAdash/CreateUser.jsx
+++ frontend/src/components/Dashboard/DBAdash/CreateUser.jsx
@@ -25,7 +25,7 @@ export default function CreateUser() {
 
   return (
     <div className="content-section">
-      <h2>Create New Admin</h2>
+      <h2>Create New User</h2>
       <form className="admin-form" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
@@ -39,15 +39,12 @@ export default function CreateUser() {
           <label htmlFor="phone">Phone:</label>
           <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
         </div>
-        <div className="form-group">
-          <label htmlFor="emp_id">Employee Id:</label>
-          <input id="emp_id" name="emp_id" value={formData.emp_id} onChange={handleChange} />
-        </div>
+        
         <div className="form-group">
           {/* <input type="checkbox" id="role" name="role" value="Role"></input>
           <label htmlFor="role">Role:</label> */}
         </div>
-        
+        <p>*Employee Id will be assigned automatically</p>
         <button type="submit" className="btn-submit">
           Create User
         </button>
